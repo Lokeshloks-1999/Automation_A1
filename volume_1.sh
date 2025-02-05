8672370ce117eb5dd748b43b0e53e67dacaf9c0f
Lokesh <lokeshloks815@gmail.com>
2025-02-05 14:02:12 -0600

    removed spaces in echo sucess and failure

--- volume_1.sh
+++ volume_1.sh
@@ -5,7 +5,7 @@ echo "The calculaated volume is:$RESULT"
 #Using If condition, to check success or failure
 if [[ $RESULT -eq 120 ]]; 
 then
-echo"Success"
+echo "Success"
 else
-echo"Failed"
+echo "Failed"
 fi
