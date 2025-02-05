709e4f79310cfbb59a0cbb83cd336c8a67bddf81
Lokesh <lokeshloks815@gmail.com>
2025-02-05 13:48:59 -0600

    Changed capital V to small c, while calling

--- volume_1.sh
+++ volume_1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # calling volume_2.sh
-RESULT=$(./Volume_2.sh)
+RESULT=$(./volume_2.sh)
 echo "The calculaated volume is: $RESULT"
 #Using If condition, to check success or failure
-if[[ $RESULT  -eq 120 ]]; then
-    echo "Success"
+if[[$RESULT-eq120]]; 
+then
+echo "Success"
 else
-    echo "Failed"
+echo "Failed"
 fi
