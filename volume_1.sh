35f79f4179728d90e8180de00c98b60ee62eb246
Lokesh <lokeshloks815@gmail.com>
2025-02-05 13:39:32 -0600

    checking it is pass or fail

--- volume_1.sh
+++ volume_1.sh
@@ -2,3 +2,9 @@
 # calling volume_2.sh
 RESULT=$(./Volume_2.sh)
 echo "The calculaated volume is: $VOLUME"
+#Using If condition, to check success or failure
+if[[ $VOLUME -eq 120 ]]; then
+    echo "Success"
+else
+    echo "Failed"
+fi
