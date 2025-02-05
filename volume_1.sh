d69a8c13a9c84af76bf5fe8b0596767dae852669
Lokesh <lokeshloks815@gmail.com>
2025-02-05 13:44:48 -0600

    Changed VOLUME to result, as I have added wrong

--- volume_1.sh
+++ volume_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # calling volume_2.sh
 RESULT=$(./Volume_2.sh)
-echo "The calculaated volume is: $VOLUME"
+echo "The calculaated volume is: $RESULT"
 #Using If condition, to check success or failure
-if[[ $VOLUME -eq 120 ]]; then
+if[[ $RESULT  -eq 120 ]]; then
     echo "Success"
 else
     echo "Failed"
