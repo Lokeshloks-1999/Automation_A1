e2a7182698718af00b1eab99f6272a9bb6438af3
Lokesh <lokeshloks815@gmail.com>
2025-02-05 14:00:18 -0600

    Leaving some spaces

--- volume_1.sh
+++ volume_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # calling volume_2.sh
 RESULT=$(./volume_2.sh)
-echo "The calculaated volume is: $RESULT"
+echo "The calculaated volume is:$RESULT"
 #Using If condition, to check success or failure
-if[[$RESULT-eq120]]; 
+if [[ $RESULT -eq 120 ]]; 
 then
 echo"Success"
 else
